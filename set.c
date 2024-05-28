cbf270c319d40782c2d1778d3422a13766c47c8e
maor <maor707070@gmail.com>
2024-05-28 17:17:04 +0300

    added union and intersection functions

--- set.c
+++ set.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#define SET_SIZE 16
 
 int main() {
-	for (int i = 0; i < 6; i++)
-		printf("%d\n", i);
+}
+
+void set_union(char *set1, char *set2, int set_size) {
+	int i;
+	for (i = 0; i < set_size; i++)
+		set1[i] = set1[i] | set2[i];
+}
+
+void set_intersect(char *set1, char *set2, int set_size) {
+	int i;
+	for (i = 0; i < set_size; i++)
+		set1[i] = set1[i] & set2[i];
 }
