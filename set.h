56a1ba0e1dccc4b06cb8cc774ee03b1c4782dd1b
maor <maor707070@gmail.com>
2024-05-30 10:58:52 +0300

    added declaration on the header

--- set.h
+++ set.h
@@ -1,5 +1,12 @@
 #define SET_H
 
-
 #define SET_SIZE 16
 
+typedef char set[SET_SIZE];
+
+void read_set(set s, int *values);
+void print_set(set s);
+void union_set(set result, set set1, set set2);
+void intersect_set(set result, set set1, set set2);
+void sub_set(set result, set set1, set set2);
+void symdiff_set(set result, set set1, set set2);
